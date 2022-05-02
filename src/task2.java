ba17dc04b0e9d8c065e66a10208d25bd7f11b8af
Ann <ann.bushl2444@gmail.com>
2022-05-02 23:19:15 +0300

    Задача 2

--- src/task2.java
+++ src/task2.java
@@ -4,10 +4,13 @@ public class task2 {
         int clientOS = 0;
         int clientDeviceYear = 2015;
         if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке.");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке.");
-        }
+            if (clientDeviceYear < 2015) {
+                  System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+            }else System.out.println("Установите версию приложения для iOS по ссылке.");}
 
-    }
+        else if(clientDeviceYear < 2015){
+                  System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        }    else System.out.println("Установите версию приложения для iOS по ссылке.");}
+
 }
+
