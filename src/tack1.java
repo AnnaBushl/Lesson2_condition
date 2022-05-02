7b92fcbcb8b3202d0285805ead1bd204bd580945
Ann <ann.bushl2444@gmail.com>
2022-05-02 20:55:53 +0300

    Initial commit

---
+++ src/tack1.java
@@ -0,0 +1,14 @@
+public class tack1 {
+        public static void main(String[] args) {
+            System.out.println("Задача 1");
+            int clientOS = 0;
+            if (clientOS == 0) {
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке.");
+            }
+
+        }
+
+
+}
---
+++ src/task2.java
@@ -0,0 +1,13 @@
+public class task2 {
+    public static void main(String[] args) {
+        System.out.println("Задача 2");
+        int clientOS = 0;
+        int clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
+
+    }
+}
